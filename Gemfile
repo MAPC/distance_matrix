9fede71b3200bbf03b5c65932d2627a5a0986a12
Matt Cloyd <cloydster@gmail.com>
2016-02-13 15:42:03 -0500

    Add test framework.
    Why:
    
    In order to ensure robust, change-resistant code, we're writing tests.
    
    This change addresses the need by:
    
    * Adding Minitest and Guard.

--- Gemfile
+++ Gemfile
@@ -5,9 +5,14 @@ ruby '2.1.5'
 gem 'json'
 gem 'logger'
 
-gem 'foreman'
+gem 'foreman', require: false
 gem 'activerecord'
+gem 'activesupport'
 gem 'pg'
 
-gem 'minitest'
-
+gem 'minitest'       # Test framework
+gem 'minitest-focus' # One test at a time
+gem 'guard'          # Autorun tests
+gem 'guard-minitest'
+gem 'rb-fsevent', require: RUBY_PLATFORM.include?('darwin') && 'rb-fsevent'
+gem 'codeclimate-test-reporter' # Test coverage
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,13 @@
 ENV['DATABASE_ENV'] = 'test'
 
-if ENV['COVERAGE']
-  puts '----> Coverage requested, but no implementation'
+require 'minitest/hell'
+require 'minitest/focus'
+
+if ENV['COVERAGE'] && ENV['CODECLIMATE_REPO_TOKEN']
+  require 'codeclimate-test-reporter'
+  CodeClimate::TestReporter.start
+  puts '----> Test coverage will be reported for this run.'
 end
 
 require 'minitest/autorun'
 require_relative '../environment'
-puts "TEST HELPER REQUIRED"
